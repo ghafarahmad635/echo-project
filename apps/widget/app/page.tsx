0f0bf56784d07bce560571772560788127a802de
ghafarahmad635 <ghafarahmad635@gmail.com>
2025-09-25 07:22:14 +0500

    Vap voice assitant

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,18 +1,21 @@
 'use client'
 
+import { useVapi } from "@/modules/widget/hooks/use-vapi";
 import { Button } from "@workspace/ui/components/button"
-import { api } from "@workspace/backend/_generated/api"
-import { useMutation, useQuery } from "convex/react";
+
 export default function Page() {
-   const users = useQuery(api.users.getmany);
-   const addUser = useMutation(api.users.addUser);
+  const { isConnected, isConnecting, isSpeaking, transcript, startCall, endCall } = useVapi();
+
   return (
     <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button onClick={() => addUser()} size="sm">Button</Button>
-       {JSON.stringify(users)}
-      </div>
+      <Button size="sm" onClick={startCall}>Start Call</Button>
+      <Button size="sm" onClick={endCall}>End Call</Button>
+      <div>
+        {isConnecting && <p>Connecting...</p>}
+        {isConnected && <p>Connected</p>}
+        {isSpeaking && <p>Speaking...</p>}
+        </div>
+        {JSON.stringify(transcript)}
     </div>
   )
 }
